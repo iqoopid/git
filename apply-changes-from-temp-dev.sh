19638228bf4906fe926f17d02cc8b1cd3c7ff51a
iqoopid <31447726+iqoopid@users.noreply.github.com>
2023-11-16 18:06:39 +0530

    Update apply-changes-from-temp-dev.sh
    STILL NEED TO UPDATE THE CODE

--- apply-changes-from-temp-dev.sh
+++ apply-changes-from-temp-dev.sh
@@ -5,3 +5,13 @@ git pull;
 git switch pytest;
 git merge --no-edit temp-dev && echo "Merge successful" || echo "Merge failed: Conflicts detected";
 git push;
+
+# CHECK THE BELOW COMMANDS AND UPDATE THE CODE
+ # 2052  git add .; git stash; git switch temp-dev; git pull; git switch pytest; git merge temp-dev; git push;
+ # 2053  git stash list
+ # 2054  git stash show stash@{0}
+ # 2055  git stash apply stash@{0}
+ # 2056  git stash list
+ # 2057  git stash drop stash@{0}
+ # 2058  history
+ 
