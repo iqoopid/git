7ef997bbf2f32f2bfe66729f84e1a71e1267799a
iqoopid <31447726+iqoopid@users.noreply.github.com>
2023-11-16 18:12:23 +0530

    Update apply-changes-from-temp-dev.sh AGAIN

--- apply-changes-from-temp-dev.sh
+++ apply-changes-from-temp-dev.sh
@@ -6,7 +6,7 @@ git switch pytest;
 git merge --no-edit temp-dev && echo "Merge successful" || echo "Merge failed: Conflicts detected";
 git push;
 
-# CHECK THE BELOW COMMANDS AND UPDATE THE CODE
+# CHECK THE BELOW COMMANDS AND UPDATE THE CODE :-
  # 2052  git add .; git stash; git switch temp-dev; git pull; git switch pytest; git merge temp-dev; git push;
  # 2053  git stash list
  # 2054  git stash show stash@{0}
@@ -15,3 +15,5 @@ git push;
  # 2057  git stash drop stash@{0}
  # 2058  history
  
+# COMMAND TO CHECK STASH DATE :-
+ # git show stash@{stash-ID} --format='%cr'
